4642077b8455f613e74bcf3f4937bd6d8e6ecff2
Farhan Rauf <farhanch101@outlook.com>
2022-10-10 09:50:03 +0000

    Update 2 files

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -6,7 +6,7 @@ import ChartBar from "./ChartBar";
 const Chart = (props) => {
   const dataPointValue = props.dataPoints.map((dataPoint) => dataPoint.Value);
   const totalMax = Math.max(...dataPointValue);
-  console.log(totalMax);
+
   return (
     <div className="chart">
       {props.dataPoints.map((datapoint) => (
